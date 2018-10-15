054a941f9fb307b138f35a73814ada89c4eee88e
Вадим Гатауллин <Vadya@macbook-pro-vadim-2.local>
2018-10-15 16:47:36 +0300

    Initial Commit

---
+++ calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by Вадим Гатауллин on 15/10/2018.
+//  Copyright © 2018 Вадим Гатауллин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
